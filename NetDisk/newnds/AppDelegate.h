9aad45eecdaa732cca8fb402cba1bd573a046ca3
fengyn <fengyn@147e8b5b-d571-564c-a00e-a70077d221f8>
2013-04-26 09:57:49 +0000

    增加一个新的Target newnds
    git-svn-id: https://192.168.1.2/svn/IOS@18 147e8b5b-d571-564c-a00e-a70077d221f8

---
+++ NetDisk/newnds/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  newnds
+//
+//  Created by fengyongning on 13-4-26.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic,retain) IBOutlet UITabBarController *myTabBarController;
+
+@end
---
+++ NetDisk/newnds/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  newnds
+//
+//  Created by fengyongning on 13-4-26.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
