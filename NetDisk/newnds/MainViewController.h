2f1135a3755bc760c2002e31cc21d6ba8ab904bb
fengyn <fengyn@147e8b5b-d571-564c-a00e-a70077d221f8>
2013-07-23 01:39:20 +0000

    add a viewController
    git-svn-id: https://192.168.1.2/svn/IOS@181 147e8b5b-d571-564c-a00e-a70077d221f8

---
+++ NetDisk/newnds/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  NetDisk
+//
+//  Created by fengyongning on 13-7-22.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
