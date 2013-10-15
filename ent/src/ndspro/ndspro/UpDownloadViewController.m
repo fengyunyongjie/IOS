ec41606fdaf92a9c62e865e08fb582a67a938e73
yangsl <yangsl@147e8b5b-d571-564c-a00e-a70077d221f8>
2013-10-15 02:02:25 +0000

    修改bug
    git-svn-id: https://192.168.1.2/svn/IOS@363 147e8b5b-d571-564c-a00e-a70077d221f8

--- ent/src/ndspro/ndspro/UpDownloadViewController.m
+++ ent/src/ndspro/ndspro/UpDownloadViewController.m
@@ -32,7 +32,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CGRect customRect = CGRectMake(0, 64, 320, 30);
+    isShowUpload = YES;
+    float topHeigth = 0;
+    if([[[UIDevice currentDevice] systemVersion] floatValue] >=7.0)
+    {
+        topHeigth = 64;
+    }
+    CGRect customRect = CGRectMake(0, topHeigth, 320, 30);
     self.customSelectButton = [[CustomSelectButton alloc] initWithFrame:customRect leftText:@"正在上传" rightText:@"正在下载" isShowLeft:isShowUpload];
     [self.customSelectButton setDelegate:self];
     [self.view addSubview:self.customSelectButton];
