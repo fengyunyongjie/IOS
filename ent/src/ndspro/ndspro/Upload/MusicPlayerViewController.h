65240d22805a8784066a31dab0acc56bd6513215
yangsl <yangsl@147e8b5b-d571-564c-a00e-a70077d221f8>
2013-10-24 10:14:10 +0000

    补交
    git-svn-id: https://192.168.1.2/svn/IOS@405 147e8b5b-d571-564c-a00e-a70077d221f8

---
+++ ent/src/ndspro/ndspro/Upload/MusicPlayerViewController.h
@@ -0,0 +1,23 @@
+//
+//  MusicPlayerViewController.h
+//  MusicPlayer
+//
+//  Created by wanghb on 12-10-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface MusicPlayerViewController : NSObject<AVAudioPlayerDelegate>
+{
+    AVAudioPlayer *newPlayer;
+    AVAudioSession *session;
+    BOOL isGoOn;
+}
+
+- (void)viewDidLoad;
+- (void)startPlay;
+- (void)stopPlay;
+
+@end
